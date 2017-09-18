9aa7bf782275293810608fcc173b60c47ca7142f
M6stafa <mostafa.najafi1996@gmail.com>
2017-09-19 03:37:13 +0430

    Create initial hierarchy

---
+++ src/components/Hello.vue
@@ -0,0 +1,16 @@
+<template>
+  <md-layout>
+    <h1>{{ msg }}</h1>
+  </md-layout>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
