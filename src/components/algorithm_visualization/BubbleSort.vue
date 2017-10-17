9b0fcd5c20036169b57551a3b50e7cc0f155e737
M6stafa <mostafa.najafi1996@gmail.com>
2017-10-18 01:07:16 +0330

    Better ui for Bubble Sort

--- src/components/algorithm_visualization/BubbleSort.vue
+++ src/components/algorithm_visualization/BubbleSort.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="bubble-sort">
-    <svg width="600" height="500">
-    </svg>
+    <svg :viewBox="`0 0 ${viewBoxWidth} ${viewBoxHeight}`" preserveAspectRatio="none meet"></svg>
   </div>
 </template>
 
@@ -14,20 +13,33 @@
 
     data() {
       return {
-        data: [12, 5, 1, 2, 8, 10],
+        viewBoxWidth: 800,
+        viewBoxHeight: 600,
+
+        data: [5, 3, 6, 4, 2, 1],
         svg: null,
 
         transitionDuration: 500,
         circleGap: 10,
-        radiusMultiplier: 5,
+        radiusMultiplier: undefined,
       };
     },
 
     mounted() {
+      // eslint-disable-next-line max-len
+      this.radiusMultiplier = (this.viewBoxWidth - ((this.data.length + 1) * this.circleGap)) / (_.sum(this.data) * 2);
+      this.viewBoxHeight = (_.max(this.data) * 2 * this.radiusMultiplier) + 100;
+
       this.svg = d3.select('svg');
-      this.update(this.data);
+      this.svg.attr('viewBox', `0 0 ${this.viewBoxWidth} ${this.viewBoxHeight}`);
+      this.update({
+        step: 0,
+        pointer: 0,
+        data: this.data,
+      });
 
       const self = this;
+
       setTimeout(() => {
         const sort = self.bubbleSort();
         const interval = setInterval(() => {
@@ -36,31 +48,39 @@
             clearInterval(interval);
             return;
           }
-          self.update(res.value.data);
+          self.update(res.value);
         }, self.transitionDuration);
       }, 1000);
     },
 
     methods: {
-      update(newData) {
+      update(data) {
+        const step = data.step;
+        const pointer = data.pointer;
+        const radiuses = data.data;
+
         const prefixSum = [];
         let sum = 0;
-        for (let i = 0; i < newData.length; i += 1) {
+        for (let i = 0; i < radiuses.length; i += 1) {
           prefixSum.push(sum);
-          sum += newData[i];
+          sum += radiuses[i];
         }
 
+        // Circles
         const circles = this.svg.selectAll('circle')
-          .data(newData, d => d);
+          .data(radiuses, d => d);
 
         circles
+          .classed('correct', (d, i) => i >= radiuses.length - step)
+          .classed('check', (d, i) => pointer !== null && (i === pointer || i === pointer + 1))
           .transition()
           .duration(this.transitionDuration)
           .attr('cx', (d, i) => this.calcCX(i, d, prefixSum[i]));
 
         circles.enter().append('circle')
+          .classed('check', (d, i) => pointer !== null && (i === pointer || i === pointer + 1))
           .attr('r', d => d * this.radiusMultiplier)
-          .attr('cy', 200)
+          .attr('cy', '50%')
           .attr('cx', (d, i) => this.calcCX(i, d, prefixSum[i]));
       },
 
@@ -72,31 +92,47 @@
 
       * bubbleSort() {
         const arr = _.cloneDeep(this.data);
-        for (let i = 0; i < arr.length - 1; i += 1) {
-          for (let j = 0; j < arr.length - 1 - i; j += 1) {
-            if (arr[j] > arr[j + 1]) {
-              [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]; // swap
-            }
+        let step = 0;
+        let p = 0; // pointer
+        for (step = 0; step < arr.length; step += 1) {
+          for (p = 0; p < arr.length - 1 - step; p += 1) {
             yield {
-              pointer: j,
+              step,
+              pointer: p,
+              data: _.cloneDeep(arr),
+            };
+
+            if (arr[p] > arr[p + 1]) {
+              [arr[p], arr[p + 1]] = [arr[p + 1], arr[p]]; // swap
+            }
+
+            yield {
+              step,
+              pointer: p,
               data: _.cloneDeep(arr),
             };
           }
+          yield {
+            step: step + 1,
+            pointer: null,
+            data: _.cloneDeep(arr),
+          };
         }
       },
     },
   };
 </script>
 
-<style scoped>
-  #bubble-sort {
-    min-width: 200px;
-    height: 500px;
-  }
-</style>
-
 <style>
   #bubble-sort svg circle {
     fill: rgba(0, 0, 255, 0.5);
+  }
+
+  #bubble-sort svg circle.correct {
+    fill: rgba(0, 255, 0, 0.5);
+  }
+
+  #bubble-sort svg circle.check {
+    fill: rgba(255, 165, 0, 0.5);
   }
 </style>
